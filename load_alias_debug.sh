2c0ff3ceaabfe8bc107cdaaa4e26c59ca4a922cf
Don Chen <126107422+Donchen22@users.noreply.github.com>
2025-08-28 14:27:29 -0700

    Update load_alias_debug.sh

--- load_alias_debug.sh
+++ load_alias_debug.sh
@@ -1,2 +1,2 @@
 kubectl get ns debug >/dev/null 2>&1 || kubectl create ns debug
-kubectl -n debug run -it $(whoami)-middleware-debug-pod --image=081731760779.dkr.ecr.us-east-1.amazonaws.com/xcloudiq/middleware-access-util:25.8.0-3 --restart=Never --rm -- bash
+kubectl -n debug run -it $(whoami)-middleware-debug-pod --image=081731760779.dkr.ecr.us-east-1.amazonaws.com/xcloudiq/middleware-access-util:latest --restart=Never --rm -- bash
